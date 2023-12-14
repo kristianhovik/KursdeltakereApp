262758d4e32614239cd15d811d448847dda3a4e9
hovik <krishovi@hiof.no>
2023-12-14 11:08:49 +0100

    La inn noen et søkefelt og en knapp og endret litt css, mye av det etter Angular kurs fra Ramazan, kommer til å endre på dette ved en senere anledning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { KursdeltakerInformasjonComponent } from './kursdeltaker-informasjon/kursdeltaker-informasjon.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: '<h1> Kursdeltakere</h1>',
+  imports: [CommonModule, RouterOutlet, KursdeltakerInformasjonComponent],
+  template: `
+  <main>
+  <header class="brand-name">
+    <img class="brand-logo" src="/assets/logo.png" alt="logo" aria-hidden="true">
+  </header>
+  <section class="content">
+    <app-kursdeltaker-informasjon></app-kursdeltaker-informasjon>
+  </section>
+</main>
+`,
+  
   /*templateUrl: './app.component.html',*/
   styleUrl: './app.component.css'
 })
--- src/app/kursdeltaker-informasjon/kursdeltaker-informasjon.component.ts
+++ src/app/kursdeltaker-informasjon/kursdeltaker-informasjon.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-kursdeltaker-informasjon',
   standalone: true,
-  imports: [],
-  templateUrl: './kursdeltaker-informasjon.component.html',
-  styleUrl: './kursdeltaker-informasjon.component.css'
+  imports: [CommonModule],
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter etter deltaker">
+      <button class="primary" type="button">Søk</button>
+    </form>
+  </section>
+`,
+  /*templateUrl: './kursdeltaker-informasjon.component.html',*/
+  /*styleUrl: './kursdeltaker-informasjon.component.css'*/
 })
 export class KursdeltakerInformasjonComponent {
 
