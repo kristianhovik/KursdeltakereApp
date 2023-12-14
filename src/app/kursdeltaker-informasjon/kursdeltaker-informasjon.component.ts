e9deafc8a04183bd17361b1c7b8ce1e86f8711e3
hovik <krishovi@hiof.no>
2023-12-14 16:46:54 +0100

    laget en liste med deltakere med tilhørende knapper, rediger og slett. I tillegg til noe uferdig css som midtstiller det så det ser ut som et skjema

--- src/app/kursdeltaker-informasjon/kursdeltaker-informasjon.component.ts
+++ src/app/kursdeltaker-informasjon/kursdeltaker-informasjon.component.ts
@@ -6,16 +6,37 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-  <section>
-    <form>
-      <input type="text" placeholder="Filter etter deltaker">
-      <button class="primary" type="button">Søk</button>
-    </form>
-  </section>
-`,
-  /*templateUrl: './kursdeltaker-informasjon.component.html',*/
-  styleUrl: './kursdeltaker-informasjon.component.css'
+    <section class="form-container">
+      <form>
+        <ul>
+          <li *ngFor="let deltaker of kursdeltakere">
+            {{ deltaker.navn }}
+            <button (click)="redigerDeltaker(deltaker)">Rediger</button>
+            <button (click)="slettDeltaker(deltaker)">Slett</button>
+          </li>
+        </ul>
+      </form>
+    </section>
+  `,
+  styleUrls: ['./kursdeltaker-informasjon.component.css']
 })
 export class KursdeltakerInformasjonComponent {
+  kursdeltakere = [
+    { id: 1, navn: 'Deltaker 1' },
+    { id: 2, navn: 'Deltaker 2' },
+    { id: 3, navn: 'Deltaker 3' },
+    { id: 4, navn: 'Deltaker 4' },
+    { id: 5, navn: 'Deltaker 5' },
+    { id: 6, navn: 'Deltaker 6' },
+  ];
 
+  redigerDeltaker(deltaker: any): void {
+    console.log('Rediger deltaker:', deltaker);
+    // Implementer redigeringen, for eksempel å åpne et redigeringsvindu eller navigere til en redigeringsrute.
+  }
+
+  slettDeltaker(deltaker: any): void {
+    console.log('Slett deltaker:', deltaker);
+    // Implementer slettingen, for eksempel å vise en bekreftelsesdialog og fjerne deltakeren fra listen.
+  }
 }
